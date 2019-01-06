9fb0aab51c093439a704f6d154e805822fd26dc3
Kamil Siwek <kamillsiwek@gmail.com>
2019-01-06 10:17:01 +0100

    First commit

---
+++ src/main/java/pl/coderslab/warsztatkoncowytaskplanner/WarsztatkoncowytaskplannerApplication.java
@@ -0,0 +1,14 @@
+package pl.coderslab.warsztatkoncowytaskplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatkoncowytaskplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarsztatkoncowytaskplannerApplication.class, args);
+    }
+
+}
+
